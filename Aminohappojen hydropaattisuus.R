fe701738daab6e69fcce3710286b182923e7c3e8
Leoliukonen <leoliukonen@gmail.com>
2026-07-07 22:01:58 +0300

    Add files via upload

---
+++ Aminohappojen hydropaattisuus.R
@@ -0,0 +1,172 @@
+#Kirjoitetaan ohjelma, joka kääntyää DNA-sekvenssin FASTA-muodosta AH-sekvenssiksi
+#Ja laskee valmistuvan proteiinin vesipakoisuuden liukuvan keskiarvon ikkunoissa
+
+#ladataan kirjastot
+library(seqinr)
+library(dplyr)
+library(ggplot2)
+
+#tallennetaan sekvenssi muuttujaan
+sequence <- read.fasta("OCA2.fasta") #Tähän transmembraanisen proteiinin mRNA-sekvenssi
+sekv <- as.data.frame(sequence)
+s <- as.vector(sekv[[1]])
+
+#käännetään sekvenssi komplementaariseksi, JOS TARVITSEE
+comp <- c()
+for (x in 1:length(s)){
+  väli <- case_when(
+   s[x] == "a" ~ "t",
+    s[x] == "t" ~ "a",
+    s[x] == "c" ~ "g",
+    s[x] == "g" ~ "c",
+   TRUE ~ "Virhe"
+  )
+  comp <- c(comp,väli)
+}
+
+#Vaihdetaan tymiini urasiiliin
+snp <- c()
+for (x in 1:length(s)){
+  väli3 <- case_when(
+    s[x] == "a" ~ "a",
+    s[x] == "t" ~ "u",
+    s[x] == "c" ~ "c",
+    s[x] == "g" ~ "g",
+    TRUE ~ "Virhe"
+  )
+  snp <- c(snp,väli3)
+}
+
+#luodaan kodoni-vektori, jossa etsitään aloituskodoni ensin
+#Alustetaan vektorit
+aloitus_kodonit <- c()
+terminaatio_kodonit <- c()
+
+for(x in seq(1:length(snp)-2)){ #käydään koko sekvenssi läpi
+  if(paste0(snp[x],snp[x+1],snp[x+2]) == "aug"){ #Etsitään kaikki aloitus-ehdokkaat
+
+    v1 <- c(as.integer(x))
+    aloitus_kodonit <- c(aloitus_kodonit,v1)
+  for(y in seq(x,length(snp),by=3)){ #etsitään aug-kodonin jälkeinen terminaatiokodoni
+    
+     if(paste0(snp[y],snp[y+1],snp[y+2]) %in% c("uaa","uag","uga")){ #Käydään läpi terminaatiokodonit
+
+       v2 <- c(as.integer(y)) # yhdistetään välimuuttujaan
+        terminaatio_kodonit <- c(terminaatio_kodonit,v2)
+        break
+        }}
+    
+ }
+
+}
+
+p1 <- length(aloitus_kodonit) #määritetään pituudet
+p2 <- length(terminaatio_kodonit)
+aloitus_kodonit <- head(aloitus_kodonit,p2) #leikataan ylimääräiset pois
+#määritetään raamien pituudet
+
+framet <- data.frame(aloitus_kodonit,terminaatio_kodonit)
+#Lisätään framet-dataframeen pituussarake
+framet <- mutate(framet, pituus = terminaatio_kodonit - aloitus_kodonit)
+
+#Valitaan frameista pisin
+longest_frame <- subset(framet,pituus == max(framet$pituus))
+
+#Luodaan kodoni-vektori
+kodoni <- c()
+
+#laitetaan kodoni-vektoriin kodonit silmukan avulla
+for (i in seq(longest_frame$aloitus_kodonit,longest_frame$terminaatio_kodonit, by=3)){
+  v3 <- c()
+  v3 <- paste0(snp[i],snp[i+1],snp[i+2])
+  kodoni <- c(kodoni,v3)
+}
+  
+#luodaan AH-vektori
+AH <- c()
+
+for (x in 1:length(kodoni)) {
+  
+  väli1 <- case_when(
+    kodoni[x] %in% c("uuu", "uuc") ~ "F",
+    kodoni[x] %in% c("uua", "uug", "cuu", "cuc", "cua", "cug") ~ "L",
+    kodoni[x] %in% c("ucu", "ucc", "uca", "ucg","agu","agc") ~ "S",
+    kodoni[x] %in% c("uau", "uac") ~ "Y",
+    kodoni[x] %in% c("ugu", "ugc") ~ "C",
+    kodoni[x] == "ugg" ~ "W",
+    kodoni[x] %in% c("ccu", "ccc", "cca", "ccg") ~ "P",
+    kodoni[x] %in% c("cau", "cac") ~ "H",
+    kodoni[x] %in% c("caa", "cag") ~ "Q",
+    kodoni[x] %in% c("aga", "agg","cgu","cgc","cga","cgg") ~ "R",
+    kodoni[x] %in% c("auu", "auc", "aua") ~ "I",
+    kodoni[x] == "aug" ~ "M", # Aloituskodoni
+    kodoni[x] %in% c("acu", "acc", "aca", "acg") ~ "T",
+    kodoni[x] %in% c("aau", "aac") ~ "N",
+    kodoni[x] %in% c("aaa", "aag") ~ "K",
+    kodoni[x] %in% c("guu", "guc", "gua", "gug") ~ "V",
+    kodoni[x] %in% c("gcu", "gcc", "gca", "gcg") ~ "A",
+    kodoni[x] %in% c("gau", "gac") ~ "D",
+    kodoni[x] %in% c("gaa", "gag") ~ "E",
+    kodoni[x] %in% c("ggu", "ggc", "gga", "ggg") ~ "G",
+    kodoni[x] %in% c("uaa", "uag", "uga") ~ "STOP",
+    TRUE ~ "Unknown" # Jos kodonia ei löydy
+  )
+    AH <- c(AH,väli1)
+}
+print(AH)
+
+#Selvitetään muodostuneen proteiinin hydrofobisuus
+hydropathy <- c()
+
+for (x in(1:length(AH))){ #Luvut ovat kyte-doolittle-asteikosta
+  väli4 <- case_when(
+  AH[x] == "L" ~ 3.8,
+  AH[x] == "V" ~ 4.2,
+  AH[x] == "I" ~ 4.5,
+  AH[x] == "F" ~ 2.8,
+  AH[x] == "C" ~ 2.5,
+  AH[x] == "M" ~ 1.9,
+  AH[x] == "A" ~ 1.8,
+  AH[x] == "G" ~ -0.4,
+  AH[x] == "T" ~ -0.7,
+  AH[x] == "S" ~ -0.8,
+  AH[x] == "W" ~ -0.9,
+  AH[x] == "Y" ~ -1.3,
+  AH[x] == "P" ~ -1.6,
+  AH[x] == "H" ~ -3.2,
+  AH[x] == "E" ~ -3.5,
+  AH[x] == "Q" ~ -3.5,
+  AH[x] == "D" ~ -3.5,
+  AH[x] == "N" ~ -3.5,
+  AH[x] == "K" ~ -3.9,
+  AH[x] == "R" ~ -4.5,
+  TRUE ~ NA
+  )
+  hydropathy <- c(hydropathy,väli4)
+}
+
+#Lasketaan hydropaattisuus 10:n liukuvissa keskiarvoissa
+ikkuna <- 16 #Liukuvann keskiarvotuksen ikkuna
+
+rolling_means <- rep(NA,ikkuna*0.5) #Täytetään tyhjät arvot NA-arvoilla
+
+for(i in 1:(length(hydropathy)-(ikkuna+1))){
+  väli5 <- mean(hydropathy[i:(i+(ikkuna-1))])
+  rolling_means <- c(rolling_means,väli5)
+}
+#Täytetään tyhjät arvot NA-arvoilla
+fill <- rep(NA,ikkuna*0.5)
+rolling_means <- c(rolling_means,fill)
+
+#Luodaan taulukko, jossa aminohappo ja hydropaattisuuden keskiarvo
+AH <- head(AH,-1)
+OAA <- seq(1:length(AH))
+hbaa <- data.frame(OAA,rolling_means)
+
+#muodostetaan kuvaaja
+plot_hydropathy <- ggplot(hbaa,aes(OAA,rolling_means))+
+  theme_bw(base_size = 15)+
+  geom_point()+
+  geom_line()+
+  labs(x = "Aminohapon järjestys", y = "Hydropaattisuuden liukuva keskiarvo")
+plot_hydropathy
